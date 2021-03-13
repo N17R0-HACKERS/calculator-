162ac1391d812b5c388a2d400c3f441fd7fd97c4
N17R0-HACKERS <72617624+N17R0-HACKERS@users.noreply.github.com>
2021-03-14 00:30:20 +0530

    Add files via upload

---
+++ calculator.sh
@@ -0,0 +1,36 @@
+﻿#-----------------------Calculator Code--------------------------#
+
+[int]$a=Read-host -Prompt 'Enter 1st Number'
+[char]$b=Read-host -Prompt 'Enter Operation You Want (+,-,*,/,%)'
+[int]$c=Read-host -Prompt 'Enter 2nd Number'
+[int]$add=$a+$c
+[int]$sub=$a-$C
+[int]$multiple=$a*$c
+[double]$divide=$a/$c
+[int]$modulus=$a%$c
+
+switch($b)
+{
+   +{$result = 'YOu Choose Addition'
+    Write-Host("Your answer is $add")
+    break;
+    }
+   -{$result = 'YOu Choose Subtraction'
+    Write-Host("Your answer is $sub")
+    break;
+   }
+   *{$result = 'YOu Choose Multiplication'
+    Write-Host("Your answer is $multiple")
+    break;
+   }
+   %{$result = 'YOu Choose Modulus'
+    Write-Host("Your answer is $modulus")
+    break;
+   }
+   /{$result = 'YOu Choose Divide'
+    Write-Host("Your answer is $divide")
+    break;
+   }
+}
+$result
+pause
